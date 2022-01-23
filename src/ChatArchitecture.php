4c978ae211e23489698682e3ae05d894365d24b7
Oghenemaro <Maro>
2022-01-23 09:10:35 -0800

    setup files

---
+++ src/ChatArchitecture.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace shadow_chat;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class ChatArchitecture implements MessageComponentInterface{
+    private $users;
+
+    public function __construct(){
+        $this->users = new \SplObjectStorage();
+    }
+
+    public function onOpen(ConnectionInterface $conn){
+        echo "New User {$conn->remoteAddress} connected \n";
+        $this->users->attach($conn);
+    }
+    public function onClose(ConnectionInterface $conn) {
+        echo "User {$conn->remoteAddress} disconnected \n";
+        $this->users->detach($conn);
+    }
+    public function onMessage(ConnectionInterface $from, $msg){
+        echo "received '$msg' from user {$from->remoteAddress}\n";
+        foreach($this->users as $user) {
+            if($user != $from){
+                $user->send($msg);
+            }
+        }
+    }
+    public function onError(ConnectionInterface $conn, \Exception $err){}
+}
+
+?>
