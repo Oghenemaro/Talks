24e9abdeda29d52d8899721c315b6917971d57cb
Oghenemaro <nerdmaro@gmail.com>
2022-01-23 09:42:15 -0800

    comment added

--- server.php
+++ server.php
@@ -3,7 +3,9 @@
 use shadow_chat\ChatArchitecture;
 require_once 'vendor/autoload.php';
 
+//create a server through the ratchet library
 $app = new \Ratchet\App('localhost', 8080, '0.0.0.0');
+//chat engine route
 $app->route('/create', new shadow_chat\ChatArchitecture);
 $app->run();
 ?>
