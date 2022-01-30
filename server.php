e2a8174bb058334307b6e9b4dde491ffac2f27d0
Maro <nerdmaro@gmail.com>
2022-01-30 14:46:09 -0800

    application server reverted to previous working state

--- server.php
+++ server.php
@@ -1,15 +1,9 @@
 <?php
 
-use shadow_chat\ChatArchitecture;
 require_once 'vendor/autoload.php';
 
-//allow us to connect to ratchet server via localhost/8080 
-//or address/port added from command line through getopt fn
-// $options = getopt('1:p:', ['listen:', 'port:']);
-// $port = $options['port'] ?? $options['p'] ?? 8080;
-// $address = $options['listen'] ?? $options['1'] ?? '127.0.0.1';
-
-$app = new \Ratchet\App('localhost', 8080, '0.0.0.0');
+$app = new \Ratchet\App('localhost', $port, $address);
 $app->route('/create', new shadow_chat\ChatArchitecture);
+$app->route('/serve', new shadow_chat\HttpArchitecture, [ '*' ]);
 $app->run();
 ?>
