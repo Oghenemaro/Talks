c387a64d833a28dd665b1dc27e883c871229a9cd
Maro <nerdmaro@gmail.com>
2022-01-30 07:40:39 -0800

    server update reversed

--- server.php
+++ server.php
@@ -5,11 +5,11 @@ require_once 'vendor/autoload.php';
 
 //allow us to connect to ratchet server via localhost/8080 
 //or address/port added from command line through getopt fn
-$options = getopt('1:p:', ['listen:', 'port:']);
-$port = $options['port'] ?? $options['p'] ?? 8080;
-$address = $options['listen'] ?? $options['1'] ?? '127.0.0.1';
+// $options = getopt('1:p:', ['listen:', 'port:']);
+// $port = $options['port'] ?? $options['p'] ?? 8080;
+// $address = $options['listen'] ?? $options['1'] ?? '127.0.0.1';
 
-$app = new \Ratchet\App('localhost', $port, $address);
+$app = new \Ratchet\App('localhost', 8080, '0.0.0.0');
 $app->route('/create', new shadow_chat\ChatArchitecture);
 $app->run();
 ?>
