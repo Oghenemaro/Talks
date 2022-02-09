d58d932572a154eae03d8e0b03f1a98d78d5602a
Maro <nerdmaro@gmail.com>
2022-02-09 06:04:13 -0800

    added border customiztion for messge received

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,22 @@
 //here a connecton instance of a websocket connection is created
-var connect = new WebSocket("ws://localhost:8080/create");
+let connect = new WebSocket("ws://localhost:8080/create");
 
 //handles message received and populates field using Jquery
-var appendMessage = function (message, sender) {
-    var status = sender ? 'Sent at' : 'Received at';
-    var contentBox = $('<div class="msg_box"><div class="msg_content"><span class="msg"></span><span class="date"></span></div> </div>');
-    contentBox.find('.date').text(new Date().toLocaleTimeString());
-    contentBox.find('.msg').text(message);
-    $('#received').prepend(contentBox);
+let appendMessage = function (message, sender) {
+    let status = sender ? 'Sent at' : 'Received at';
+    console.log(status);
+    let owner = $('<div class="msg_box"><div class="msg_content msg_content_owner"><span class="msg"></span><span class="date"></span></div> </div>');
+    let recepient = $('<div class="msg_box msg_box_recipient"><div class="msg_content msg_content_recepient"><span class="msg"></span><span class="date"></span></div> </div>');
+    let msgSender = (status === 'Sent at') ? owner : recepient;
+    msgSender.find('.date').text(new Date().toLocaleTimeString());
+    msgSender.find('.msg').text(message);
+    $('#received').prepend(msgSender);
 }
 connect.onOpen = function(){
     console.log('connection created');
-    // setInterval(function () {
-    //     connect.send('ping');
-    // }, 60000);
+    setInterval(function () {
+        connect.send('ping');
+    }, 60000);
 }
 
 // message is logged here
@@ -25,17 +28,17 @@ connect.onmessage = function (event){
 //Reconnect if connection is closed abruptly
 connect.onclose = function(){
     console.log('connection closed');
-    // console.error(e);
-    // clearInterval();
-    // setInterval(function(){
-    //     connect = new WebSocket('ws://localhost:8080/create');
-    // }, 5000);
+    console.error(e);
+    clearInterval();
+    setInterval(function(){
+        connect = new WebSocket('ws://localhost:8080/create');
+    }, 5000);
 }
 
 
 $(document).ready(function(){
     $('#submit').click(function(){
-        var message = $('#message').val();
+        let message = $('#message').val();
         if(message){
             console.log('sending message: " ' + message + ' " ');
             connect.send(message);
